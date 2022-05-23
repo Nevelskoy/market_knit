4cf8839487a2408ef8638184b2a96e24de4f595b
Alex Feliksovich <39111114+Nevelskoy@users.noreply.github.com>
2022-05-23 20:34:43 +0300

    feat: add userprof

--- market_knit/urls.py
+++ market_knit/urls.py
@@ -7,7 +7,7 @@ from django.urls import include
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', mainapp.main, name='main'),
     path('list_of_accommodations/', include('mainapp.urls', namespace='acc')),
 ]
