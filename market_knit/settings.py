7e526eaf52842399e1c01ed1f7e9cc8c28b0f1c4
Alex Feliksovich <39111114+Nevelskoy@users.noreply.github.com>
2022-05-23 20:34:43 +0300

    feat: add authapp

--- market_knit/settings.py
+++ market_knit/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = 'authapp.TravelUser'
 
 
 # Static files (CSS, JavaScript, Images)
